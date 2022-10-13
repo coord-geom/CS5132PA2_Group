94a08ef9edc36ee7961ec2a1ed6e1ea063ed5a04
AirrowSST <airrowsst@gmail.com>
2022-10-14 06:18:27 +0800

    Added BTree class and created empty test class

---
+++ src/BTree.java
@@ -0,0 +1,13 @@
+/**
+ * Implementation of a B Tree
+ * Utilises the BNode class
+ */
+
+public class BTree<T> {
+
+
+
+    public BTree() {
+
+    }
+}
---
+++ src/tests/BNodeTester.java
@@ -0,0 +1,8 @@
+package tests;
+
+//TODO make actual content
+public class BNodeTester {
+    public static void test_insertion() {
+
+    }
+}
