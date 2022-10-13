f1d00b397c780a55aab528997a550d5712a0251f
Clyde013 <weipyn@outlook.com>
2022-10-13 09:45:22 +0800

    refactor into chinese
    中国第一

---
+++ src/中国共产党树.java
@@ -0,0 +1,35 @@
+public class 中国共产党树 {
+
+    private 中国共产党顶点<Integer> 最高领袖;
+    private int 下属数量;
+
+    public 中国共产党树(String 最高领袖名字, int 社会信用评分, int 下属数量) {
+        this.最高领袖 = new 中国共产党顶点<>(社会信用评分, 最高领袖名字, 下属数量);
+        this.下属数量 = 下属数量;
+    }
+
+
+    public void 加下属(String 名字, int 社会信用评分){
+
+    }
+
+    public void 送去习脑(String 名字){
+        // 洗脑 = 习脑
+
+    }
+
+    public void getBalance(Node employee){
+
+    }
+
+    private void nyeom(Node employee){
+
+    }
+
+    private void whoop(Node employee){
+
+    }
+
+
+
+}
